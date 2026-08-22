af248bb62892483483d19349f3bb8a3a5cb3dcc0
Pushpraj Mishra <pushparajsonu1@gmail.com>
2026-08-22 13:50:38 +0530

    migration from program.cs

--- DevopsLabApi/Program.cs
+++ DevopsLabApi/Program.cs
@@ -14,7 +14,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
